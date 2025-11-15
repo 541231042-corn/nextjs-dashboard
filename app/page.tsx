84f3a2d4c20365c4202465bd1e51d5d9149fb5db
cornel12-hen <noelmarvell6@gmail.com>
2025-11-15 11:46:07 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import clsx from 'clsx';
+import Image from 'next/image';
+
+function Invoice({status}: {status: 'paid' | 'pending'}){
+  return(
+    <div className={clsx(
+      'flex items-center justify-between rounded-lg p-4',
+      status === 'paid' ? 'bg-green-100' : 'bg-yellow-100')}>
+        <p>Tes</p>
+    </div>
+  )
+}
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        {<AcmeLogo />}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -26,8 +38,25 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+          src="/hero-desktop.png"
+          alt="Hero Image"
+          width={1000}
+          height={760}
+          priority
+          className='hidden md:block'
+        ></Image>
+        <Image
+          src="/hero-mobile.png"
+          alt="Hero Image"
+          width={375}
+          height={285}
+          priority
+          className='md:hidden'
+        ></Image>
         </div>
       </div>
+    <Invoice status='pending'/>
     </main>
   );
 }
---
+++ app/ui/fonts.tsx
@@ -0,0 +1,6 @@
+import {Inter} from 'next/font/google';
+import {Montserrat, Lusitana} from 'next/font/google';
+
+export const montserrat = Montserrat({ subsets: ['latin'], weight: ['400', '700'] });
+export const inter = Inter({ subsets: ['latin'], weight: ['800', '700'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
